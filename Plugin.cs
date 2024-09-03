544907d3cf900f2e114b4a405e06b7a47ffc8ca7
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-09-03 19:39:59 -0300

    Removed acceptor feature bc Crispy+ already has it

--- Plugin.cs
+++ Plugin.cs
@@ -7,13 +7,12 @@ namespace TweaksPlus
     [BepInPlugin("pixelguy.pixelmodding.baldiplus.tweaksplus", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-		internal static ConfigEntry<bool> enableItemAcceptorCheck, enableAutoMapFillCheck;
+		internal static ConfigEntry<bool> enableAutoMapFillCheck;
         private void Awake()
         {
 			Harmony h = new("pixelguy.pixelmodding.baldiplus.tweaksplus");
 			h.PatchAll();
 
-			enableItemAcceptorCheck = Config.Bind(mainSec, "Enable Item Acceptors display", true, "If True, every time you look at an IITemAcceptor object (such as a Coin Door) with the required item, the interaction display will popup in the screen");
 			enableAutoMapFillCheck = Config.Bind(mainSec, "Enable map forced fillup", true, "If True, for any room that you enter, all the map tiles will be forcefully revealed, regardless of their shape.");
         }
 
