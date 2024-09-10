6513be694a8ad3ad4d216c54c8639589c9961576
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-09-10 13:29:25 -0300

    You can click in the pitstop

---
+++ Patches/PitstopManagerPatch.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace TweaksPlus.Patches
+{
+	[HarmonyPatch(typeof(PitstopGameManager))]
+	internal class PitstopManagerPatch
+	{
+		[HarmonyPatch("BeginPlay")]
+		static void Postfix()
+		{
+			if (Plugin.enableItemUsageInPitstop.Value)
+				Singleton<CoreGameManager>.Instance.GetPlayer(0).itm.Disable(false); // No u
+		}
+	}
+}
--- Plugin.cs
+++ Plugin.cs
@@ -7,7 +7,8 @@ namespace TweaksPlus
     [BepInPlugin("pixelguy.pixelmodding.baldiplus.tweaksplus", PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
     public class Plugin : BaseUnityPlugin
     {
-		internal static ConfigEntry<bool> enableAutoMapFillCheck, enableChalklesInstaDisable, enableChalklesProportionalSpawn;
+		internal static ConfigEntry<bool> enableAutoMapFillCheck, enableChalklesInstaDisable, enableChalklesProportionalSpawn,
+			enableItemUsageInPitstop;
         private void Awake()
         {
 			Harmony h = new("pixelguy.pixelmodding.baldiplus.tweaksplus");
@@ -16,6 +17,7 @@ namespace TweaksPlus
 			enableAutoMapFillCheck = Config.Bind(mainSec, "Enable map forced fillup", true, "If True, for any room that you enter, all the map tiles will be forcefully revealed, regardless of their shape.");
 			enableChalklesInstaDisable = Config.Bind(mainSec, "Enable Chalkles instant disable", true, "If True, the moment you leave a room with Chalkles will instantly reset him (like it used to be before v0.4).");
 			enableChalklesProportionalSpawn = Config.Bind(mainSec, "Enable Chalkles proportional spawn", true, "If True, Chalkles\' spawn time will be proportional to the estimated size of the room it is currently in.");
+			enableItemUsageInPitstop = Config.Bind(mainSec, "Enable item usage in the Pitstop", true, "If True, the player will be able of using any item from their inventory in the shop (feature implemented since 0.7)");
         }
 
 		const string mainSec = "Tweak Settings";
