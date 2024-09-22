bc5b784931cde543ba5c3b4b701f0f0d30ef7ac5
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-09-22 15:24:54 -0300

    Quite better method.. although not perfect

---
+++ Comps/ChalkFaceTimerFix.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TweaksPlus.Comps
+{
+	internal class ChalkFaceTimerFix : MonoBehaviour
+	{
+		void FixedUpdate()
+		{
+			if (!changeTimer) return;
+
+			chalkles.audMan.audioDevice.time = chalkles.audMan.audioDevice.clip.length * (chalkles.charge / chalkles.setTime);
+		}
+
+		public void SetTimerFix(bool timer) => changeTimer = timer;
+
+		bool changeTimer = false;
+
+		[SerializeField]
+		public ChalkFace chalkles;
+	}
+}
--- Patches/ChalkFacePatch.cs
+++ Patches/ChalkFacePatch.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using TweaksPlus.Comps;
 using UnityEngine;
 
 namespace TweaksPlus.Patches
@@ -6,18 +7,41 @@ namespace TweaksPlus.Patches
 	[HarmonyPatch]
     class ChalkFacePatch
     {
+		[HarmonyPatch(typeof(ChalkFace), "Initialize")]
+		[HarmonyPrefix]
+		static void AddComponent(ChalkFace __instance) =>
+			__instance.gameObject.AddComponent<ChalkFaceTimerFix>().chalkles = __instance;
+
 		[HarmonyPatch(typeof(ChalkFace_Idle), "Enter")]
 		[HarmonyPostfix]
 		static void ActuallyCancelMaBoy(ChalkFace ___chalkles)
 		{
 			if (Plugin.enableChalklesInstaDisable.Value)
 				___chalkles.charge = 0; // Literally resets it
+
+			___chalkles.GetComponent<ChalkFaceTimerFix>().SetTimerFix(false);
 		}
 
 		[HarmonyPatch(typeof(ChalkFace_Charging), "Enter")]
-		[HarmonyPostfix]
-		static void AdaptTimer(ChalkFace ___chalkles, Chalkboard ___chalkboard) =>
-			___chalkles.setTime = Mathf.Max(1f, Mathf.Sqrt((___chalkboard.Room.size.x ^2) + (___chalkboard.Room.size.z ^2)) * 1.65f);
-		
-    }
+		[HarmonyPrefix]
+		static void AdaptTimer(ChalkFace ___chalkles, Chalkboard ___chalkboard)
+		{
+			if (Plugin.enableChalklesProportionalSpawn.Value)
+			{
+				float prevTimer = ___chalkles.setTime;
+				___chalkles.setTime = Mathf.Max(15f, Mathf.Sqrt((___chalkboard.Room.size.x ^ 2) + (___chalkboard.Room.size.z ^ 2)) * 1.65f);
+				if (___chalkles.charge > 0f)
+					___chalkles.charge -= prevTimer - ___chalkles.setTime;
+
+				___chalkles.GetComponent<ChalkFaceTimerFix>().SetTimerFix(true);
+			}
+			
+		}
+
+		[HarmonyPatch(typeof(ChalkFace_Laughing), "Enter")]
+		[HarmonyPrefix]
+		static void DisableTimerFix(ChalkFace ___chalkles) =>
+			___chalkles.GetComponent<ChalkFaceTimerFix>().SetTimerFix(false);
+
+	}
 }
