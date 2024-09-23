4fa557695259bcd802b2afcf6923bc3a3619f075
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-09-23 16:01:13 -0300

    Npcs actually wander around detention

--- Patches/NPCPatch.cs
+++ Patches/NPCPatch.cs
@@ -1,6 +1,7 @@
 ﻿using HarmonyLib;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 namespace TweaksPlus.Patches
 {
@@ -16,7 +17,9 @@ namespace TweaksPlus.Patches
 
 			IEnumerator WaitToLeave()
 			{
-				__instance.Navigator.Entity.SetFrozen(true);
+				yield return null;
+				var detention = new NavigationState_DetentionState(__instance, 99, __instance.ec.CellFromPosition(__instance.transform.position).room);
+				__instance.behaviorStateMachine.ChangeNavigationState(detention);
 				float cooldown = 15f;
 				while (cooldown > 0f)
 				{
@@ -24,11 +27,34 @@ namespace TweaksPlus.Patches
 					yield return null;
 				}
 
-				__instance.Navigator.Entity.SetFrozen(false);
+				detention.End();
 				yield break;
 			}
 
 			__instance.Navigator.Entity.StartCoroutine(WaitToLeave());
+		}
+	}
+
+	internal class NavigationState_DetentionState(NPC npc, int priority, RoomController office) : NavigationState(npc, priority)
+	{
+		readonly RoomController office = office;
+		public override void Enter()
+		{
+			base.Enter();
+			npc.Navigator.FindPath(office.RandomEventSafeCellNoGarbage().FloorWorldPosition);
+			
+		}
+
+		public override void DestinationEmpty()
+		{
+			base.DestinationEmpty();
+			npc.Navigator.FindPath(office.RandomEventSafeCellNoGarbage().FloorWorldPosition);
+		}
+
+		public void End()
+		{
+			priority = 0;
+			npc.behaviorStateMachine.RestoreNavigationState();
 		}
 	}
 }
