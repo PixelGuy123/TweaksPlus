174139f92a089b0ccbba40cdab694b52408fcbde
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2024-09-23 07:12:51 -0300

    Mrs Pomp uses the modifiable factor

--- Patches/NoLateTeacherPatch.cs
+++ Patches/NoLateTeacherPatch.cs
@@ -11,7 +11,7 @@ namespace TweaksPlus.Patches
 		static void AdaptTimer(ref float ___classTime, RoomController ___classRoom, DijkstraMap ___dijkstraMap, SoundObject[] ___audNumbers)
 		{
 			if (Plugin.enableMrsPompDynamicTimer.Value)
-				___classTime = Mathf.Clamp(60 * Mathf.RoundToInt((___dijkstraMap.Value(___classRoom.doors[0].aTile.position) * 5.7f) / 60f), 60f, 60 * (___audNumbers.Length - 1));
+				___classTime = Mathf.Clamp(60 * Mathf.RoundToInt((___dijkstraMap.Value(___classRoom.doors[0].aTile.position) * Plugin.mrsPompTimerFactor.value) / 60f), 60f, 60 * (___audNumbers.Length - 1));
 		}
 	}
 }
