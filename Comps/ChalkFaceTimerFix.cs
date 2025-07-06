5ce23a6e2bbb4004c61d4d0670b9de92d5cd51d8
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2025-07-06 17:10:30 -0300

    Update ChalkFaceTimerFix.cs

--- Comps/ChalkFaceTimerFix.cs
+++ Comps/ChalkFaceTimerFix.cs
@@ -4,11 +4,16 @@ namespace TweaksPlus.Comps
 {
 	internal class ChalkFaceTimerFix : MonoBehaviour
 	{
+		void Awake()
+		{
+			if (!chalkles) return;
+			maxClipLength = chalkles.audSpawn.subDuration;
+		}
 		void FixedUpdate()
 		{
-			if (!changeTimer) return;
+			if (!changeTimer || maxClipLength == -1f) return;
 
-			float targetTime = chalkles.audMan.audioDevice.clip.length * (chalkles.charge / chalkles.setTime);
+			float targetTime = maxClipLength * (chalkles.charge / chalkles.setTime);
 			float currentTime = chalkles.audMan.audioDevice.time;
 
 			if (Mathf.Abs(currentTime - targetTime) > float.Epsilon) // Avoids flickering by adding a *small* time gap
@@ -20,8 +25,7 @@ namespace TweaksPlus.Comps
 		public void SetTimerFix(bool timer) => changeTimer = timer;
 
 		bool changeTimer = false;
-
-		[SerializeField]
+		float maxClipLength = -1f;
 		public ChalkFace chalkles;
 	}
 }
