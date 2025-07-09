056f0300f3c140f5ffaa005fb92fa06be5af6dea
PixelGuy123 <89644121+PixelGuy123@users.noreply.github.com>
2025-07-09 14:27:28 -0300

    Fixed chalkles spawn not being properly changed

--- Comps/ChalkFaceTimerFix.cs
+++ Comps/ChalkFaceTimerFix.cs
@@ -4,29 +4,25 @@ namespace TweaksPlus.Comps
 {
 	internal class ChalkFaceTimerFix : MonoBehaviour
 	{
-		void FixedUpdate()
-		{
-			if (!changeTimer || maxClipLength == -1f) return;
-
-			float targetTime = maxClipLength * (chalkles.charge / chalkles.setTime);
-			float currentTime = chalkles.audMan.audioDevice.time;
-
-			if (Mathf.Abs(currentTime - targetTime) > smallInterval) // Prevents flickering by adding a *small* time gap
-			{
-				chalkles.audMan.audioDevice.time = targetTime;
-			}
-		}
-
 		public void SetTimerFix(bool timer)
 		{
 			changeTimer = timer;
-			maxClipLength = chalkles.setTime;
+			maxClipLength = chalkles.audSpawn.subDuration;
+
+			if (changeTimer && maxClipLength > 0f && chalkles.setTime > 0f)
+			{
+				// pitch = (audio length) / (charge duration)
+				float targetPitch = maxClipLength / chalkles.setTime;
+				chalkles.audMan.pitchModifier = Mathf.Clamp(targetPitch, 0.1f, 2f);
+			}
+			else
+			{
+				chalkles.audMan.pitchModifier = 1f; // Reset pitch if not syncing
+			}
 		}
 
 		bool changeTimer = false;
 		float maxClipLength = -1f;
-		[SerializeField]
-		float smallInterval = 0.05f;
 		public ChalkFace chalkles;
 	}
 }
